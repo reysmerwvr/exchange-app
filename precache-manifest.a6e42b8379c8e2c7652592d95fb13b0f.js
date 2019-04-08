37e2f1278230652e867f6e757e7cfee679a776c2
Reysmer Valle <reysmerwvr@gmail.com>
2019-04-08 15:15:15 -0500

    Updates

---
+++ precache-manifest.a6e42b8379c8e2c7652592d95fb13b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exchange-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43d78a810dc729a1c474",
+    "url": "/exchange-app/static/js/runtime~main.80d10873.js"
+  },
+  {
+    "revision": "47165693abebee6d153d",
+    "url": "/exchange-app/static/js/main.83eea0e3.chunk.js"
+  },
+  {
+    "revision": "1cc5cf0b146ec543ca4f",
+    "url": "/exchange-app/static/js/2.b7214964.chunk.js"
+  },
+  {
+    "revision": "47165693abebee6d153d",
+    "url": "/exchange-app/static/css/main.b7a6f137.chunk.css"
+  },
+  {
+    "revision": "1cc5cf0b146ec543ca4f",
+    "url": "/exchange-app/static/css/2.de4c5ade.chunk.css"
+  },
+  {
+    "revision": "58945ea16b5adb65c32c2b8004d5c6ee",
+    "url": "/exchange-app/index.html"
+  }
+];
